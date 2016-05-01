2b57b9eda3055ae15c6bceefe5020f3390e498ac
Ninja Banjo <ninja@ninjabanjo.guru>
2016-04-30 23:55:36 -0500

    add registerCallback function and give tick function a job

--- app/utils/tick-handler-spec.js
+++ app/utils/tick-handler-spec.js
@@ -1,17 +1,45 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import TickHandler from './tick-handler';
 
-describe('utils/TickHandler', () => {
+describe('TickHandler', () => {
   describe('constructor', () => {
-    // doesnt really do nuthin right now
+    it('should create an empty `callbacks` array', () => {
+
+    });
   });
 
   describe('tick', () => {
-    it('should do nothing', () => {
-      TickHandler.prototype.tick.call(null);
+    it('should call each array item in `callbacks` on context as a function',  () => {
+      const spy1 = sinon.spy();
+      const spy2 = sinon.spy();
+      const contextStub = {
+        callbacks: [spy1,spy2]
+      };
 
-      expect('everything').to.be.OK; // shh. it's okay
+      TickHandler.prototype.tick.call(contextStub);
+
+      expect(spy1).to.have.been.calledOnce;
+      expect(spy2).to.have.been.calledOnce;
     });
   });
+
+  describe('registerCallback', () => {
+    it('should throw when the provided is not a function', () => {
+      expect(TickHandler.prototype.registerCallback.bind({}, {})).to.throw(Error);
+    });
+
+    it('should push the provided function onto the callback array on context', () => {
+      const func = function(){};
+      const contextStub = {
+        callbacks: []
+      };
+
+      TickHandler.prototype.registerCallback.call(contextStub, func);
+
+      expect(contextStub.callbacks.length).to.equal(1);
+      expect(contextStub.callbacks[0]).to.equal(func);
+    });
+  })
 });
--- app/utils/tick-handler.js
+++ app/utils/tick-handler.js
@@ -1,9 +1,15 @@
 class TickHandler {
   constructor() {
-    
+    this.callbacks = [];
   }
 
   tick() {
+    this.callbacks.map(e => e());
+  }
+
+  registerCallback(cb) {
+    if(typeof cb !== 'function') throw new Error('`cb` must be a function');
+    this.callbacks.push(cb);
   }
 }
 
